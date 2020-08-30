09801ce1298b5d31013237d1746a37f2acf48608
livnekes <livne.kestin@forter.com>
2020-08-30 17:21:47 +0300

    try to add mutations

--- backend/services/metrics-service/src/resolvers/Request.ts
+++ backend/services/metrics-service/src/resolvers/Request.ts
@@ -1,11 +1,28 @@
-import { Resolver, Query, Ctx } from 'type-graphql';
+import { Resolver, Query, Ctx, Mutation, Arg } from 'type-graphql';
 import { Request } from '../db/entity/Request';
 import { Context } from './types/context';
+import { NewRequestInput } from '../dto/new-request.input';
 
 @Resolver()
 export class RequestResolver {
   @Query(() => [Request])
   requests(@Ctx() { tenantId }: Context) {
     return Request.find({ where: { tenantId } });
+  }
+
+  @Mutation(() => Request)
+  async addRequest(@Ctx() { tenantId }: Context, @Arg('newRequestData') newRequestData: NewRequestInput): Promise<Request> {
+    const request = Request.create({
+      tenantId,
+      ...newRequestData,
+    });
+    return await request.save();
+  }
+
+  @Mutation(() => Boolean)
+  async removeRequest(@Ctx() { tenantId }: Context, @Arg('id') id: number) {
+    const request = await Request.find({ where: { tenantId, id } });
+    await Request.remove(request);
+    return true;
   }
 }
