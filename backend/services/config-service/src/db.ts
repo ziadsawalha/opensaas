6a6fb722211b4a18e9553ea1c7c58560ad1aa222
livnekes <livne.kestin@forter.com>
2020-09-02 15:36:53 +0300

    add config service readme

--- backend/services/config-service/src/db.ts
+++ backend/services/config-service/src/db.ts
@@ -3,7 +3,7 @@ import { connection, connect } from 'mongoose';
 export default (uri: string) => {
   const dbConnect = async () => {
     try {
-      await connect(uri, { useNewUrlParser: true });
+      await connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
       console.info(`Successfully connected to ${uri}`);
     } catch (error) {
       console.error(`Error connecting to database with uri ${uri} and error: `, error);
