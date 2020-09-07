93f4e6b7592d34c10c1c4bf6c22dca6918a55435
livnekes <livne.kestin@forter.com>
2020-09-07 15:54:57 +0300

    add deploy script

--- cli/cli.ts
+++ cli/cli.ts
@@ -2,18 +2,20 @@
 
 import * as yargs from 'yargs';
 import { startServices } from './run-services';
+import { deployServices } from './deploy-services';
 import { addService } from './add-service';
 import { removeService } from './remove-service';
 import { initRepo } from './init-repository';
 
 const commands: any = {
   run: startServices,
+  deploy: deployServices,
   init: initRepo,
   add: addService,
   remove: removeService,
 };
 
-const COMMANDS = ['run', 'add', 'remove'];
+const COMMANDS = ['run', 'add', 'remove', 'deploy'];
 
 function getCommandAndArgs(argv: any) {
   const args = argv._;
